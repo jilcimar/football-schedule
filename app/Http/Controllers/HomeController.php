8bccb92a9c372eea24eb1c8c81639adb37fe61e9
jilcimar <jilcimarfernandes@rits.com.br>
2020-08-27 08:39:50 -0300

    agrupamento por mes no dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Subscriber;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
@@ -28,6 +29,20 @@ class HomeController extends Controller
         $usersActive = Subscriber::all()->count();
         $usersAll = count(Telegram::getUpdates());
 
-        return view('home', compact('usersActive','usersAll'));
+        $labelMeses = [];
+        $dadosMeses = [];
+
+        $groupByMonth = Subscriber::select('id', 'created_at')
+            ->get()
+            ->groupBy(function($date) {
+                return Carbon::parse($date->created_at)->format('m');
+            });
+
+        foreach ($groupByMonth as $key=> $data) {
+            $date = gregoriantojd($key,1,2020);
+            $labelMeses[] = jdmonthname($date,0);
+            $dadosMeses[] = count($data);
+        }
+        return view('home', compact('usersActive','usersAll', 'labelMeses','dadosMeses'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,4 +58,87 @@
         </div>
         <!-- /.col -->
     </div>
+
+    <!-- charts -->
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-6">
+            <div class="card card-danger">
+                <div class="card-header">
+                    <h3 class="card-title">Representação dos usuários</h3>
+                </div>
+                <div class="card-body">
+                    <canvas id="chartUsersActive" width="400" height="400"></canvas>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-12 col-sm-6 col-md-6">
+            <div class="card card-success">
+                <div class="card-header">
+                    <h3 class="card-title">Inscrições por mês</h3>
+                </div>
+                <div class="card-body">
+                    <canvas id="chartUsersMonth" width="400" height="400"></canvas>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
+        <!-- /.col -->
+    </div>
 @stop
+@section('js')
+    <script>
+        $(function () {
+            var usersActive = {!!$usersActive!!};
+            var usersAll = {!! $usersAll !!};
+            var usersDrop = {!!$usersAll-$usersActive!!};
+
+            var ctx = document.getElementById("chartUsersActive").getContext('2d');
+            var chartUsersActive = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: ["Usuários Ativos", "Já usaram o bot", "Usuários Perdidos"],
+                    datasets: [{
+                        backgroundColor: ["#3cba9f","#3e95cd", "#ef1624"],
+                        data: [usersActive,usersAll,usersDrop]
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                }
+            });
+
+            var labelMeses = {!! json_encode($labelMeses) !!}
+            var dadosMeses = {!! json_encode($dadosMeses) !!}
+            console.log(labelMeses);
+            var cty = document.getElementById("chartUsersMonth").getContext('2d');
+            var chartUsersMonth = new Chart(cty, {
+                type: 'bar',
+                data: {
+                    labels: labelMeses,
+                    datasets: [
+                        {
+                            label: "Agrupamento de inscritos ativos por mês",
+                            backgroundColor: [
+                                "#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9",
+                                "#c45850","#3e95cd", "#8e5ea2","#3cba9f",
+                                "#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9",
+                            ],
+                            data: dadosMeses
+                        },
+                    ]
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    scales: {
+                        yAxes:
+                            [{ticks: {beginAtZero: true}}]
+                    }
+                }
+            });
+        });
+    </script>
+@endsection
