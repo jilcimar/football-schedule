952c4a3faf439710c4b3a2e5a0dfb6eea99f5412
jilcimar <jilcimarfernandes@rits.com.br>
2020-10-09 08:53:29 -0300

    salvando os usuários que interagem com o bot e voltando com os horarios automaticamente

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Conversation\JogosDeAmanha;
 use App\Conversation\JogosDeHoje;
 use App\Conversation\StartConversation;
+use App\Models\Subscriber;
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\BotMan\Users\User;
 use BotMan\Drivers\Telegram\TelegramDriver;
 
 
@@ -17,6 +19,7 @@ class BotManController extends Controller
      * @var BotMan $bot
      */
     public $bot;
+    public $user;
 
     public function setUp()
     {
@@ -29,6 +32,7 @@ class BotManController extends Controller
         DriverManager::loadDriver(TelegramDriver::class);
 
         $this->bot = BotManFactory::create($config);
+        $this->user = new User();
     }
 
     public function __construct()
@@ -39,15 +43,60 @@ class BotManController extends Controller
     public function handle()
     {
         $this->bot->hears('/start', function (BotMan $bot) {
-            $bot->startConversation(new StartConversation);
+            try {
+                $user = $this->bot->getUser();
+                Subscriber::updateOrCreate(
+                    [
+                        'chat_id' => $user->getId(),
+                    ],
+                    [
+                        'chat_id' =>  $user->getId(),
+                        'username' =>$user->getUsername(),
+                        'first_name' => $user->getFirstName(),
+                    ]
+                );
+                $bot->startConversation(new StartConversation);
+            } catch (\Exception $e) {
+                //
+            }
         });
 
         $this->bot->hears('/jogosdehoje', function (BotMan $bot) {
-            $bot->startConversation(new JogosDeHoje);
+            try {
+                $user = $this->bot->getUser();
+                Subscriber::updateOrCreate(
+                    [
+                        'chat_id' => $user->getId(),
+                    ],
+                    [
+                        'chat_id' =>  $user->getId(),
+                        'username' =>$user->getUsername(),
+                        'first_name' => $user->getFirstName(),
+                    ]
+                );
+                $bot->startConversation(new JogosDeHoje);
+            } catch (\Exception $e) {
+                //
+            }
         });
 
         $this->bot->hears('/jogosamanha', function (BotMan $bot) {
-            $bot->startConversation(new JogosDeAmanha);
+            try {
+                $user = $this->bot->getUser();
+                Subscriber::updateOrCreate(
+                    [
+                        'chat_id' => $user->getId(),
+                    ],
+                    [
+                        'chat_id' =>  $user->getId(),
+                        'username' =>$user->getUsername(),
+                        'first_name' => $user->getFirstName(),
+                    ]
+                );
+                $bot->startConversation(new JogosDeAmanha);
+            } catch (\Exception $e) {
+                //
+            }
         });
 
         $this->bot->listen();
